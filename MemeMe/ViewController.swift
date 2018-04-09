d5b5a9b06154372d608c706ce04c5534fac3dbe4
Luiz Arantes Sa <LArantesSa@icloud.com>
2018-04-09 04:33:17 -0500

    Initial Commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Luiz Arantes Sa on 4/9/18.
+//  Copyright © 2018 Luiz Arantes Sa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
